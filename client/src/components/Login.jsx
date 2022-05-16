59a4f1c639884cc94accbeb637985863f0af0340
Amar Omerhodzic <omerhodzic.amar.21@size.ba>
2022-05-16 23:09:10 +0200

    commit

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,21 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
 
+ // const {email, setEmail} = useState("");
+ // const {password, setPassword} = useState("");
+
+ /* const login = () => {
+    axios.post("http://localhost:3000/login", {
+      email: email,
+      password: password,
+    }).then((response) => {
+      console.log(response);
+    });
+  };
+*/
 
   return (
     <div>
@@ -22,20 +35,36 @@ const Login = () => {
           <div className="col-md-6 p-5">
             <h1 className="display-6 fw-bolder mb-5">PRIJAVA</h1>
             <form>
+
               <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email adresa</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <input type="email" 
+               // onChange={(e) => {
+               //   setEmail(e.target.value);
+               // }}
+                class="form-control" 
+                id="exampleInputEmail1" 
+                aria-describedby="emailHelp" />
                 <div id="emailHelp" class="form-text">Ne dijelite svoj email.</div>
               </div>
+
               <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Šifra</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" />
+                <input type="password" 
+               // onChange={(e) => {
+               //   setPassword(e.target.value);
+                //}}
+                class="form-control" 
+                id="exampleInputPassword1" />
               </div>
+
               <div class="mb-3 form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1" />
                 <label class="form-check-label" for="exampleCheck1">Čekiraj</label>
               </div>
-              <button type="submit" class="btn btn-primary">Potvrda</button>
+              <button type="submit" 
+             // onClick={login}
+              class="btn btn-primary">Potvrda</button>
             </form>
           </div>
         </div>
--- server/server.js
+++ server/server.js
@@ -1,20 +1,28 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 //App config
 const app = express();
+app.use(cors());
+app.use(express.json());
 console.log('hi');
 const port = process.env.PORT || 8001
 const connection_url = `mongodb+srv://Amar:ZOG2tNpCoFKLVej5@cluster0.osup3.mongodb.net/Obdaniste-db?retryWrites=true&w=majority`
 
 
-//Midlewares
+
 
 // DB config
 mongoose.connect(connection_url);
 
 //API Endpoints
 app.get('/', (req,res) => res.status(200).send("Obdaniste Radost"));
+
+/*
+app.post("/login",(req,res)=>{ 
+    
+});*/
 
 
 //listener
