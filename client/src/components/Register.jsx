f930b736351f10e953607172316a1120487e8d4d
hamza-starcevic <starcevic.hamza.21@size.ba>
2022-05-26 18:53:14 +0200

    Hamza: Nepotpuno implementirrao post api

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,35 +1,114 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Axios from "axios";
 const Register = () => {
+  const [ime, setIme] = useState("");
+  const [prezime, setPrezime] = useState("");
+  const [imeRoditelja, setImeRoditelja] = useState("");
+  const [datumRodjenjaDjeteta, setDatumRodjenjaDjeteta] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [brojTelefona, setBrojTelefona] = useState("");
+  const [roditeljiZaposleni, setRoditeljiZaposleni] = useState(true);
+  const [adresaStanovanja, setAdresaStanovanja] = useState("");
+  const [gradStanovanja, setGradStanovanja] = useState("");
+  const [dijeteBezRoditelja, setDijeteBezRoditelja] = useState(false);
+  const [samohraniILIrazvedeni, setSamohraniILIrazvedeni] = useState("");
+  const [rom, setRom] = useState(true);
+  const [bitneNapomene, setBitneNapomene] = useState("");
+
+  const createUser = (event) => {
+    event.preventDefault();
+    console.log("hi");
+    Axios.post("http://localhost:8001/createUser", {
+      ime: ime,
+      prezime: prezime,
+      imeRoditelja: imeRoditelja,
+      datumRodjenjaDjeteta: datumRodjenjaDjeteta,
+      email: email,
+      password: password,
+      brojTelefona: brojTelefona,
+      roditeljiZaposleni: roditeljiZaposleni,
+      adresaStanovanja: adresaStanovanja,
+      gradStanovanja: gradStanovanja,
+      dijeteBezRoditelja: dijeteBezRoditelja,
+      samohraniILIrazvedeni: samohraniILIrazvedeni,
+      rom: rom,
+      bitneNapomene: bitneNapomene,
+    });
+  };
+
   return (
     <form style={{ padding: "4%", backgroundColor: "#cce1ff", color: "black" }}>
       <div class="form-group">
-        <label for="exampleFormControlInput1">Ime</label>
-        <input type="ime" class="form-control" placeholder="Ime" />
+        <label>Ime</label>
+        <input
+          class="form-control"
+          placeholder="Ime"
+          onChange={(event) => {
+            setIme(event.target.value);
+          }}
+        />
       </div>
       <div class="form-group">
-        <label for="exampleFormControlInput1">Prezime</label>
-        <input type="ime" class="form-control" placeholder="Prezime" />
+        <label>Prezime</label>
+        <input
+          class="form-control"
+          placeholder="Prezime"
+          onChange={(event) => {
+            setPrezime(event.target.value);
+          }}
+        />
       </div>
       <div class="form-group">
-        <label for="exampleFormControlInput1">Ime Roditelja</label>
-        <input type="ime" class="form-control" placeholder="Ime Roditelja" />
+        <label>Ime Roditelja</label>
+        <input
+          class="form-control"
+          placeholder="Ime Roditelja"
+          onChange={(event) => {
+            setImeRoditelja(event.target.value);
+          }}
+        />
       </div>
       <div class="form-group">
-        <label for="exampleFormControlInput1">Datum rodjenja djeteta</label>
-        <input type="ime" class="form-control" placeholder="npr. dd/mm/gggg" />
+        <label>Datum rodjenja djeteta</label>
+        <input
+          class="form-control"
+          placeholder="npr. dd/mm/gggg"
+          onChange={(event) => {
+            setDatumRodjenjaDjeteta(event.target.value);
+          }}
+        />
       </div>
       <div class="form-group">
-        <label for="exampleFormControlInput1">E-mail</label>
-        <input type="ime" class="form-control" placeholder="E-mail" />
+        <label>E-mail</label>
+        <input
+          class="form-control"
+          placeholder="E-mail"
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        />
       </div>
       <div class="form-group">
-        <label for="exampleFormControlInput1">Password</label>
-        <input type="ime" class="form-control" placeholder="Password" />
+        <label>Password</label>
+        <input
+          class="form-control"
+          placeholder="Password"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
       </div>
       <div class="form-group">
-        <label for="exampleFormControlInput1">Broj telefona</label>
-        <input type="ime" class="form-control" placeholder="Broj telefona" />
+        <label>Broj telefona</label>
+        <input
+          type="ime"
+          class="form-control"
+          placeholder="Broj telefona"
+          onChange={(event) => {
+            setBrojTelefona(event.target.value);
+          }}
+        />
       </div>
       <div class="form-check">
         <input
@@ -37,26 +116,32 @@ const Register = () => {
           type="checkbox"
           value=""
           id="roditeljiZaposleni"
+          onChange={(event) => {
+            setRoditeljiZaposleni(event.target.value);
+          }}
         />
-        <label class="form-check-label" for="defaultCheck1">
-          Roditelji zaposleni?
-        </label>
+        <label class="form-check-label">Roditelji zaposleni?</label>
       </div>
       <div class="form-group">
-        <label for="exampleFormControlInput1">Adresa stanovanja</label>
+        <label>Adresa stanovanja</label>
         <input
           type="ime"
           class="form-control"
           placeholder="Adresa stanovanja"
+          onChange={(event) => {
+            setAdresaStanovanja(event.target.value);
+          }}
         />
       </div>
       <div class="form-group">
-        <label for="exampleFormControlInput1">Grad stanovanja</label>
-        <input type="ime" class="form-control" placeholder="Grad stanovanja" />
-      </div>
-      <div class="form-group">
-        <label for="exampleFormControlInput1">Broj telefona</label>
-        <input type="ime" class="form-control" placeholder="Broj telefona" />
+        <label>Grad stanovanja</label>
+        <input
+          class="form-control"
+          placeholder="Grad stanovanja"
+          onChange={(event) => {
+            setGradStanovanja(event.target.value);
+          }}
+        />
       </div>
 
       <div class="form-check">
@@ -65,6 +150,9 @@ const Register = () => {
           type="checkbox"
           value=""
           id="roditeljiZaposleni"
+          onChange={(event) => {
+            setDijeteBezRoditelja(event.target.value);
+          }}
         />
         <label class="form-check-label" for="defaultCheck1">
           Dijete bez roditelja?
@@ -75,21 +163,41 @@ const Register = () => {
           class="form-check-input"
           type="checkbox"
           value=""
-          id="roditeljiZaposleni"
+          id="rom"
+          onChange={(event) => {
+            setRom(event.target.value);
+          }}
         />
+
         <label class="form-check-label" for="defaultCheck1">
           Rom?
         </label>
       </div>
-
+      <div class="form-group">
+        <label>Samohrani ili razvedeni roditelji?</label>
+        <input
+          class="form-control"
+          placeholder="Samohrani/Razvedeni"
+          onChange={(event) => {
+            setSamohraniILIrazvedeni(event.target.value);
+          }}
+        />
+      </div>
       <div class="form-group">
         <label for="exampleFormControlTextarea1">Bitne napomene</label>
         <textarea
           class="form-control"
           id="exampleFormControlTextarea1"
           rows="3"
+          onChange={(event) => {
+            setBitneNapomene(event.target.value);
+          }}
         ></textarea>
       </div>
+      <button class="btn btn-primary" onClick={createUser}>
+        {" "}
+        Registrujte se{" "}
+      </button>
     </form>
   );
 };
--- server/server.js
+++ server/server.js
@@ -1,28 +1,34 @@
 const express = require("express");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const UserModel = require('./models/Users.js')
-const userRoutes = require('./routes/userRoutes')
-const { errorHandler, notFound } = require('./middleware/errorMiddleware')
+const UserModel = require("./models/Users.js");
+const userRoutes = require("./routes/userRoutes");
+const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 //App config
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-console.log('hi');
-const port = process.env.PORT || 8001
-const connection_url = `mongodb+srv://Amar:ZOG2tNpCoFKLVej5@cluster0.osup3.mongodb.net/Obdaniste-db?retryWrites=true&w=majority`
-
-
-
+console.log("hi");
+const port = process.env.PORT || 8001;
+const connection_url = `mongodb+srv://Amar:ZOG2tNpCoFKLVej5@cluster0.osup3.mongodb.net/Obdaniste-db?retryWrites=true&w=majority`;
 
 // DB config
 mongoose.connect(connection_url);
-app.use('/api/users',userRoutes);
+app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 //API Endpoints
+
+app.post("/createUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json(user);
+});
+
 //Probao sam i get i post rade u thunderu/postman
 /*app.get('/login', (req,res) => {
      UserModel.find({}, (err,result) => {
@@ -48,7 +54,6 @@ app.use(errorHandler);
         res.send(user);
     }
 });*/
-
 
 //listener
 app.listen(port, () => console.log(`Server radi na: ${port}`));
